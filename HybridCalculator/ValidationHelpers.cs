aa19e47611d83548f59e168599656b6399d7d59a
nextgen <dgnextgen@gmail.com>
2016-11-18 12:21:08 +0000

    New validation HelperMethod

---
+++ HybridCalculator/ValidationHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HybridCalculator
+{
+    class ValidationHelpers
+    {
+        public static int ValidateFlatES(int input, int lowValue, int highValue)
+        {
+            if (input < lowValue)
+            {
+                throw new ArgumentOutOfRangeException("It's garbage, go get some new armour");
+            }
+            else if (input > highValue)
+            {
+                throw new ArgumentOutOfRangeException("That's not an acceptable answer, are you drunk?");
+            }
+            return input;
+        }
+    }
+}
